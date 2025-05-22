466f0a0fdfb156d9beda36bdf024bbcab0ea5938
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-05-22 15:09:19 +0530

    humber fix

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -160,7 +160,7 @@ const Navbar = () => {
 
           <div className=" flex items-center">
             <LanguageSelector />
-            <button onClick={() => setMobileOpen(!mobileOpen)} className="ml-4">
+            <button onClick={() => setMobileOpen(!mobileOpen)} className="lg:hidden ml-4">
               {mobileOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
             </button>
           </div>
