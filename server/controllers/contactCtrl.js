b8761ad84f8819ca06eab50d1d63aa925a4b19f7
Rishi <rishimaheshwari040@gmail.com>
2025-06-12 14:14:50 +0530

    update

--- server/controllers/contactCtrl.js
+++ server/controllers/contactCtrl.js
@@ -6,7 +6,7 @@ const contactCtrl = async (req, res) => {
 
     try {
         const emailRes = await mailSender(
-            "rishimaheshwari040@gmail.com",
+            "astrovishal1008@gmail.com",
 
             "Your Data send successfully",
             contactUsEmail(name, email, contact, message)
